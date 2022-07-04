73224390dc196c07bf622608c6dcb17fb94fbede
Ash Winter <ash_winter@hotmail.co.uk>
2022-07-04 16:10:24 +0100

    Chapter 13 - Make It

--- src/main/java/com/javadevelopersguide/junit/Bank.java
+++ src/main/java/com/javadevelopersguide/junit/Bank.java
@@ -2,6 +2,6 @@ package com.javadevelopersguide.junit;
 
 public class Bank {
     public Money reduce(Expression source, String to) {
-        return Money.dollar(10);
+        return source.reduce(to);
     }
 }
--- src/main/java/com/javadevelopersguide/junit/Expression.java
+++ src/main/java/com/javadevelopersguide/junit/Expression.java
@@ -1,4 +1,7 @@
 package com.javadevelopersguide.junit;
 
 public interface Expression {
+
+    Money reduce(String to);
+
 }
