f001ebd0a722bc68498388b6a58cff1a365686f1
Ash Winter <ash_winter@hotmail.co.uk>
2022-07-05 14:27:35 +0100

    Chapter 14 - Change

--- src/test/java/com/javadevelopersguide/junit/TheMoneyExampleTest.java
+++ src/test/java/com/javadevelopersguide/junit/TheMoneyExampleTest.java
@@ -2,7 +2,8 @@ package com.javadevelopersguide.junit;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class TheMoneyExampleTest {
 
@@ -65,5 +66,19 @@ public class TheMoneyExampleTest {
         Money result = bank.reduce(Money.dollar(1), "USD");
         assertEquals(Money.dollar(1), result);
     }
+
+    @Test
+    public void testReduceMoneyDifferentCurrency() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
+    @Test
+    public void testIdentityRate() {
+        assertEquals(1, new Bank().rate("USD", "USD"));
+    }
+
 
 }
